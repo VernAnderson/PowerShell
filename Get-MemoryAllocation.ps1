5fbe1e62b14cebe47e614eca8ca19fb609538eb0
Vern Anderson <keencommander@hotmail.com>
2016-05-12 11:02:17 -0500

    Update Get-MemoryAllocation.ps1

--- Get-MemoryAllocation.ps1
+++ Get-MemoryAllocation.ps1
@@ -1,4 +1,25 @@
-﻿Function  Get-MemoryAllocation
+<#
+.Synopsis
+    Generates Hyper-V memory allocation report
+.DESCRIPTION
+    This script will generate VM resource allocation information to provide an idea of how much you have used and what is still available
+.EXAMPLE
+    Get-MemoryAllocation
+.EXAMPLE
+    Or just run it from ISE, you can also just paste the whole thing at the PowerShell prompt and press enter twice afterwards
+.INPUTS
+    NONE
+.OUTPUTS
+    A list of Hypervisor or cluster resoucres as well as resources used by each VM with the total per Hypervisor
+.NOTES
+    This is by no means complete and is a work in progress
+.ROLE
+    Microsoft Hyper-V
+.FUNCTIONALITY
+    Hyper-V Resource Allocation
+#>
+
+Function  Get-MemoryAllocation
 {
 Param ([Parameter(Mandatory=$true,Position=0)]$ComputerName)
 Get-WmiObject -ComputerName $ComputerName Win32_PhysicalMemory  | Measure-Object Capacity -Sum  | Format-Table -AutoSize @{Name=$ComputerName+" Total(GB)";Expression={$_.Sum / 1GB}; Format="F2"; Alignment="Right"}
