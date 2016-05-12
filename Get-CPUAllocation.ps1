e892e35571aa6b431918fba1f48f0ce7e5ed4002
Vern Anderson <keencommander@hotmail.com>
2016-05-12 11:01:32 -0500

    Update Get-CPUAllocation.ps1

--- Get-CPUAllocation.ps1
+++ Get-CPUAllocation.ps1
@@ -1,4 +1,25 @@
-﻿Function Get-CPUAllocation
+<#
+.Synopsis
+    Generates Hyper-V CPU allocation report
+.DESCRIPTION
+    This script will generate VM resource allocation information to provide an idea of how much you have used and what is still available
+.EXAMPLE
+    Get-CPUAllocation
+.EXAMPLE
+    Or just run it from ISE, you can also just paste the whole thing at the PowerShell prompt and press enter twice afterwards
+.INPUTS
+    NONE
+.OUTPUTS
+    A list of Hypervisor or cluster resoucres as well as resources used by each VM with the total per Hypervisor
+.NOTES
+    This is by no means complete and is a work in progress
+.ROLE
+    Microsoft Hyper-V
+.FUNCTIONALITY
+    Hyper-V Resource Allocation
+#>
+
+Function Get-CPUAllocation
 {
 Param ([Parameter(Mandatory=$true,Position=0)]$ComputerName)
 $UsedCPU = 0
