3e2a667cae9e1f26185f869a92cbef9b7e59c00f
Vern Anderson <keencommander@hotmail.com>
2019-03-07 07:08:51 -0600

    Update Get-LoggedOnUser.ps1

--- Get-LoggedOnUser.ps1
+++ Get-LoggedOnUser.ps1
@@ -1,4 +1,4 @@
 Get-WmiObject -Query "SELECT * from Win32_Process WHERE Caption LIKE 'explorer.exe'" | ForEach-Object {$_.GetOwner().User}
 
-# After writing this I discovered the parrameter for Get-Process explorer.exe -IncludeUserName
-# However the WMI script doesn't require elevation and can run as a user so I'll keep this
+# After writing this I discovered the parameter for Get-Process explorer.exe -IncludeUserName
+# However, the WMI script doesn't require elevation and can run as a user so I'll keep this
