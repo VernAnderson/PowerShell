996a674819aa53d0e64b54d92da075e75d53c2bf
Vern Anderson <keencommander@hotmail.com>
2022-04-06 07:12:52 -0500

    Add files via upload
    Set Windows NTP time sync to best practices for Active Directory

---
+++ Set-NTPTime.ps1
@@ -0,0 +1,17 @@
+﻿$DomainRole = (Get-WmiObject Win32_ComputerSystem).DomainRole
+switch ($DomainRole)
+    {
+    0 {"Stand Alone Workstation" ; $SYNCType = "MANUAL"}
+    1 {"Member Workstation" ; $SYNCType = "DOMHIER"}
+    2 {"Standalone Server" ; $SYNCType = "MANUAL"}
+    3 {"Member Server" ; $SYNCType = "DOMHIER"}
+    4 {"Backup Domain Controller" ; $SYNCType = "DOMHIER"}
+    5 {"Primary Domain Controller" ; $SYNCType = "MANUAL"}
+    }
+
+if ($SYNCType -ne $Null)
+    {
+    $FilePath = Get-ChildItem C:\Windows\system*\w32tm.exe -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName
+    cmd.exe /c "$FilePath /config /update /manualpeerlist:time.nist.gov /syncfromflags:$SYNCType"
+    }
+cmd.exe /c "$FilePath /dumpreg /subkey:Parameters"
