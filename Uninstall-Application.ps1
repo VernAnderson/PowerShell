96cfd75af14d6cd37f620aeb2d6e3520faf2c333
Vern Anderson <keencommander@hotmail.com>
2023-05-11 15:46:52 -0500

    added comment based help
    While adding comment based help wound up rewriting the entire script to leverage out-gridview

--- Uninstall-Application.ps1
+++ Uninstall-Application.ps1
@@ -1,5 +1,14 @@
-#This example uninstalled Skype from my machine
- 
-#The following should all be on one line . . .
- 
-Get-WmiObject Win32_Product | Where-Object {$_.IdentifyingNumber -eq "{1845470B-EB14-4ABC-835B-E36C693DC07D}"} |  Invoke-WmiMethod -Name "Uninstall"
+<#
+.Synopsis
+   Removes an application or software product from Windows
+.DESCRIPTION
+   I ran into a server running Windows Core and there were no GUI tools such as appwiz.cpl so I had to figure this out. This script will generate a list and display it in "grid view" and whicever applications you select will be sent down the pipeline to the 
+uninstall method defined by that application
+.EXAMPLE
+   .\Uninstall-Application.ps1
+.OUTPUTS
+   This script does not really display any output
+.NOTES
+   I still need to test this version
+#>
+Get-WmiObject Win32_Product | Out-GridView -Title "Select the application you wish to unistall from Windows" -PassThru | Invoke-WmiMethod -Name "Uninstall" -WhatIf
