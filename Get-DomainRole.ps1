c92889a7d699d350cf534357580600112c17ed2b
Vern Anderson <keencommander@hotmail.com>
2023-05-11 15:31:26 -0500

    added comment based help

--- Get-DomainRole.ps1
+++ Get-DomainRole.ps1
@@ -1,3 +1,15 @@
+<#
+.Synopsis
+   Gets the domain role of the current computer
+.DESCRIPTION
+   This script will get the domain role property from a WMI class, which is a number system. It translates that number to a human readable string based on the documentation from Microsoft as it relates to that property and number.
+.EXAMPLE
+   .\Get-DomainRole.ps1
+.OUTPUTS
+   The output is a human readable string of the computers domain role. For example Primary Domain Controller if that computer holds the PDC Emulater FSMO role
+.NOTES
+   This script is also a great example of a real world use for the "switch" construct in PowerShell
+#>
 $DomainRole = (Get-WmiObject Win32_ComputerSystem).DomainRole
 switch ($DomainRole) {
 0 {"Stand Alone Workstation"}
