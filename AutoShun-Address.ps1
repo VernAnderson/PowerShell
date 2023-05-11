404c32291ab0d2fc69f7a9c0f3ab78aa6704d856
Vern Anderson <keencommander@hotmail.com>
2023-05-11 12:09:38 -0500

    Added help block and renamed the FW rule
    removed Rackspace from the firewall rule name and added help

--- AutoShun-Address.ps1
+++ AutoShun-Address.ps1
@@ -1,4 +1,18 @@
-﻿# Change the value of the following variable to control how many login failures before you block
+<#
+.Synopsis
+   "Blocks an IP in the Windows Firewall that has failed a given number of logon attempts"
+.DESCRIPTION
+   "This command retrieves failed Remote logon events from the Security log and gets the offending IP Address from the logged event. Then is uses that IP to create a firewall rule to block further connections from that IP Address."
+.EXAMPLE
+   "You can schedule a Windows Scheduled Task to run this script as often as you like (5 minutes is recommended)"
+.EXAMPLE
+   "You may want to have that scheduled task run as the ""SYSTEM"" Account"
+.OUTPUTS
+   "The output of this script is the full syntax needed by the netsh command, and a firewall rule named ""FailedLogon-AutoShun"" gets created (or you can change that string to whatever you would like)"
+.NOTES
+   "This script is provided for free without any implied support"
+#>
+# Change the value of the following variable to control how many login failures before you block
 $MaxFailures=3
 
 # We get the offending IP from the Eventlog
@@ -27,7 +41,7 @@ Function Create-Rule
     {
     if ($ShunAddresses)
         {
-        netsh advfirewall firewall add rule name="Rackspace-AutoShun" dir=in action=block remoteip=$ShunAddresses enable=yes
+        netsh advfirewall firewall add rule name="FailedLogon-AutoShun" dir=in action=block remoteip=$ShunAddresses enable=yes
         }
     }
    
@@ -36,13 +50,13 @@ Function Add-Rule
     {
     if ($ShunAddresses)
         {
-        netsh advfirewall firewall set rule name="Rackspace-AutoShun" dir=in new remoteip=$ShunAddresses enable=yes
+        netsh advfirewall firewall set rule name="FailedLogon-AutoShun" dir=in new remoteip=$ShunAddresses enable=yes
         }
     }
 
 
 # Determine if the rule already exist or not
-netsh advfirewall firewall show rule Name=Rackspace-Autoshun
+netsh advfirewall firewall show rule Name=FailedLogon-Autoshun
 if ($LASTEXITCODE -gt 0)
     {
     Create-Rule
