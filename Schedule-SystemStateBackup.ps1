293946651821345c1f0db307944203a391733d17
Vern Anderson <keencommander@hotmail.com>
2023-05-11 15:59:19 -0500

    added comment based help

--- Schedule-SystemStateBackup.ps1
+++ Schedule-SystemStateBackup.ps1
@@ -1,3 +1,18 @@
+<#
+.Synopsis
+   Adds a Windows Scheduled tasks to create a System State backup that can be picked up by the normal file system backup for disaster and recovery purposes
+.DESCRIPTION
+   This command will generate the proper syntax to send to a scheduled task that will then create a system state backup to a UNC path but tricking it to be saved to the local drive for the file backup to pickup and save for OS recovery and disaster recovery 
+purposes.
+.EXAMPLE
+   .\Schedule-SystemStateBackup.ps1
+.EXAMPLE
+   
+.OUTPUTS
+   This command's output is to send the proper syntax and parameters to a scheduled task that runs the wbadmin command for a system state backup to a UNC path
+.NOTES
+   Disaster Recovery is important and if you don't have a system state you will not recover all the OS configuration and settings without it
+#>
 # Setup variables
 $server = $env:computername
 $UNCpath = "\\$server\Backup" # Create a shared folder and put the full UNC path here
@@ -12,4 +27,4 @@ Set-Location $ENV:SystemRoot\System32
     if ($wbadmin -eq $True) {
     .\wbadmin.exe enable backup -addtarget:$UNCpath -schedule:02:00 -user:Administrator -password:P@ssw0rd1 -systemState -quiet
     } 
-    else {cls ; Write-Warning "Windows Backup is not properly installed" ; exit} 
+    else {cls ; Write-Warning "Windows Backup is not properly installed" ; exit}
