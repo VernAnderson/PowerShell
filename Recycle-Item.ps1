9894909538f55676c9892dbe3d1813ab26428ea1
Vern Anderson <keencommander@hotmail.com>
2023-05-11 16:08:10 -0500

    added comment based help

--- Recycle-Item.ps1
+++ Recycle-Item.ps1
@@ -1,3 +1,15 @@
+<#
+.Synopsis
+   Sends the object in the Path variable to the Windows Recycle bin instead of permanantly deleting it
+.EXAMPLE
+   Recycle-Item -Path C:\TEMP\*.del
+.EXAMPLE
+   Recycle-Item C:\Temp\DELEATME.txt
+.OUTPUTS
+   This script does not generate any output
+.NOTES
+   I may be improving this script in the future
+#>
 function Recycle-Item ($Path)
     {
     $objShell = New-Object -ComObject Shell.Application
