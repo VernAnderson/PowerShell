8fe180c2edc4cebdcbca339a9fc5126db1bf6c50
Vern Anderson <keencommander@hotmail.com>
2023-05-11 15:37:23 -0500

    added comment based help

--- Get-IISLogFilePath.ps1
+++ Get-IISLogFilePath.ps1
@@ -1,3 +1,15 @@
+<#
+.Synopsis
+   Discovers the log file path of each site in IIS
+.DESCRIPTION
+   Returns the current path of the IIS Log file path for each site inside of IIS
+.EXAMPLE
+   .\Get-IISLogFilePath.ps1
+.OUTPUTS
+   Outputs the full path where each website's log files are being saved to disk
+.NOTES
+   IIS was a big part of my job for over 10 years or more
+#>
 Function Get-IISLogFilePath
 {
 # Load the PowerShell Module for IIS
