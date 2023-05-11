0a8039fa9b05908bcf9e6018e526af37d3cda706
Vern Anderson <keencommander@hotmail.com>
2023-05-11 15:06:38 -0500

    Update Disable-ServerManager.ps1
    added comment based help

--- Disable-ServerManager.ps1
+++ Disable-ServerManager.ps1
@@ -1,1 +1,13 @@
+<#
+.Synopsis
+   Stops Server Manager from running every time you log on
+.DESCRIPTION
+   This one liner will delete the Windows Scheduled Task that causes Server Manager to run upon logon. If you find that annoying this is the PowerShell way to fix it.
+.EXAMPLE
+   Get-ScheduledTask ServerManager | Disable-ScheduledTask
+.OUTPUTS
+   This script does not generate any output
+.NOTES
+   This is not a script per say it is more of a one liner
+#>
 Get-ScheduledTask ServerManager | Disable-ScheduledTask
