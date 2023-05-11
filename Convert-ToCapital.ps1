ac2d160ea7fb61fe97eadb800410851e4bd612ce
Vern Anderson <keencommander@hotmail.com>
2023-05-11 12:54:46 -0500

    Update Convert-ToCapital.ps1
    Adding comment block help

--- Convert-ToCapital.ps1
+++ Convert-ToCapital.ps1
@@ -1,3 +1,15 @@
+.Synopsis
+   "Converts the first charecter in a string to a capital letter"
+.DESCRIPTION
+   "Breaks a string of alpha characters apart and joins it back together making the first character upper case or ""Title"" case"
+.EXAMPLE
+   Convert-ToCapital joe
+.EXAMPLE
+   Convert-ToCapital "superDuper"
+.OUTPUTS
+   "The function will output a string with the first letter being upper case"
+.NOTES
+   "This script was just a fun project if it were complete it would include checks to insure the string is not numeric"
 function Convert-ToCapital ($String="vern"){
 [string]$1stLetter = $String[0]
 $CapLetter = $1stLetter.ToUpper()
