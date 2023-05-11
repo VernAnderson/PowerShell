af07d9fd2ec261e39016009715584bd791a53df7
Vern Anderson <keencommander@hotmail.com>
2023-05-11 15:13:54 -0500

    Update Disable-WindowsFirewall.ps1
    added comment based help

--- Disable-WindowsFirewall.ps1
+++ Disable-WindowsFirewall.ps1
@@ -1,3 +1,18 @@
+<#
+.Synopsis
+   Affectively disables the Windows Firewall
+.DESCRIPTION
+   You should never stop the Windows Firewall Services. Instead it is recommended that you disable the profiles. I do not remember the side affects caused by disabling the service but this is the recommended way. It should only be done if you are behind a ph
+ysical firewall or some other device that secures your network ports and access.
+.EXAMPLE
+   .\Disable-WindowsFirewall.ps1
+.EXAMPLE
+   Import-Module NetSecurity ; Get-NetFirewallProfile | Set-NetfirewallProfile -Enabled False
+.OUTPUTS
+   This script generates no output unless you include the "-PassThru" switch on the end of the "set-netfirewallprofile" command
+.NOTES
+   This is less of a script, it's really just a one liner
+#>
 Import-Module NetSecurity ; Get-NetFirewallProfile | Set-NetfirewallProfile -Enabled False
 
 # This also works by itself
