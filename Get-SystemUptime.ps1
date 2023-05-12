2eaeabe62d1259e430f6d4e2073a25ef14c9fd16
Vern Anderson <keencommander@hotmail.com>
2023-05-12 08:18:45 -0500

    added comment based help

--- Get-SystemUptime.ps1
+++ Get-SystemUptime.ps1
@@ -1,1 +1,15 @@
+<#
+.Synopsis
+   Gets the current uptime in days, hours, and minutes
+.DESCRIPTION
+   This script queries a performance counter WMI object for the current system uptime
+.EXAMPLE
+   .\Get-SystemUptime.ps1
+.EXAMPLE
+   New-TimeSpan -Seconds (Get-WmiObject Win32_PerfFormattedData_PerfOS_System).SystemUptime | Format-Table Days,Hours,Minutes
+.OUTPUTS
+   This script will display the number of days, and how many hours and minutes the current computer has been up and running
+.NOTES
+   This can also just be a one liner but it is very useful so I saved it here
+#>
 New-TimeSpan -Seconds (Get-WmiObject Win32_PerfFormattedData_PerfOS_System).SystemUptime | Format-Table Days,Hours,Minutes
