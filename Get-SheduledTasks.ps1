e9495c05ef1395e48c2b4d38c32b28bce0618b09
Vern Anderson <keencommander@hotmail.com>
2023-05-12 08:27:57 -0500

    added comment based help

--- Get-SheduledTasks.ps1
+++ Get-SheduledTasks.ps1
@@ -1,3 +1,17 @@
+<#
+.Synopsis
+   This script gets scheduled tasks in older versions of Windows
+.DESCRIPTION
+   This script queries and XML file that used to be used by Windows for scheduled tasks. Microsoft has since changed that and PowerShell now has CMDLETs build in for scheduled tasks
+.EXAMPLE
+   .\Get-SheduledTasks.ps1
+.EXAMPLE
+   Get-ScheduledTask #The newer built in CMDLET by Microsoft
+.OUTPUTS
+   Outputs the task name, whether it is enabled or disabled, the "triggers", the next scheduled run time, and the author
+.NOTES
+   This script no longer works on Windows newer than 2012 or Windows 7
+#>
 $ErrorActionPreference = 'SilentlyContinue'
 Set-Location $ENV:SystemRoot\System32\Tasks
 Get-ChildItem | Where-Object {!$_.PSIsContainer} |
