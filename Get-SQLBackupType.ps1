692e9a37f22d1f8a7f3c7131d44f5cfe7867a351
Vern Anderson <keencommander@hotmail.com>
2023-05-12 08:44:42 -0500

    added comment based help

--- Get-SQLBackupType.ps1
+++ Get-SQLBackupType.ps1
@@ -1,3 +1,18 @@
+<#
+.Synopsis
+   Gets MS SQL Server backup events and determines the most likely type of backup
+.DESCRIPTION
+   This script looks for EVENT IDs in the Windows Application Logs related to MS SQL Backup events and determines whether the backup type was to disk, or to tape, or to a Network UNC path.
+.EXAMPLE
+   .\Get-SQLBackupType.ps1
+.EXAMPLE
+   
+.OUTPUTS
+   This outputs the backup type to the screen translated from a number to human readable types based on a Microsoft Article that describes those numbers
+.NOTES
+   I originally wrote this script when a customer was breaking the SQL backup chain because they were performing their own backups outside of the scheduled backups to tape. This script helped me prove that the backup failures were not the fault of the backup
+    to tape software, but rather the customers own manual backups that broke the "chain" for SQL Transaction Log backups.
+#>
 $Database = Read-Host "Enter the name of the database"
 $lastfail = Get-EventLog -LogName Application -Source *SQL* -Message *Backup*$Database*  | Where-Object {$_.EventID -eq 3041} | Select-Object -First 1 -ExpandProperty TimeGenerated
 $18264 = Get-EventLog -LogName Application -Source *SQL* -After $lastfail -Data *$Database* | Where-Object {$_.EventID -eq 18264}
