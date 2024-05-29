1d9b0f81b4ccea8a70056b671b943dd581a17467
Vern Anderson <keencommander@hotmail.com>
2024-05-29 09:55:10 -0500

    Add files via upload

---
+++ Get-PathGUI.ps1
@@ -0,0 +1,33 @@
+﻿$folder = Add-Type -AssemblyName System.Windows.Forms
+$FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog -Property @{
+    RootFolder      = "MyComputer"
+    Description     = "Pick a folder"   
+}
+    if($FolderBrowser.ShowDialog() -eq "OK")
+    {
+        $folder += $FolderBrowser.SelectedPath
+    }
+Get-ChildItem -Name $FolderBrowser.SelectedPath -Directory
+
+<#
+## Valid values for RootFolder
+	ApplicationData
+	CommonApplicationData
+	LocalApplicationData
+	Cookies
+	DesktopDirectory
+	Favorites
+	History
+	InternetCache
+	MyComputer
+	MyDocuments
+	MyMusic
+	MyPictures
+	MyVideos
+	Recent
+	SendTo
+	StartMenu
+	Startup
+	System
+	Templates
+#>
