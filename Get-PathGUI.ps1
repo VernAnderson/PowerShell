f65e01868d902a67cf3ead68dc4cc37743777133
Vern Anderson <keencommander@hotmail.com>
2024-06-03 14:37:12 -0500

    Update Get-PathGUI.ps1

--- Get-PathGUI.ps1
+++ Get-PathGUI.ps1
@@ -33,3 +33,19 @@ Get-ChildItem -Name $FolderBrowser.SelectedPath -Directory
 	System
 	Templates
 #>
+
+<#
+On the desktop website:
+
+There are two ways to access your saved posts:
+
+Go to https://www.facebook.com/help/220284408163249.
+On your Facebook homepage, look for Saved in the left sidebar menu under the Explore section.
+Clicking on either option will take you to your saved items page, which displays all your saved posts and collections.
+
+Additional tips:
+
+You can use the filter option on the saved items page to sort your saved content by type (posts, links, events, etc.).
+To remove a post from your saved list, tap the three-dot icon next to the post and select Unsave.
+Facebook allows you to create collections to organize your saved posts into categories. You can find the option to create new collections on both the mobile app and desktop website.
+#>
