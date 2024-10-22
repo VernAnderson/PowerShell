00edab0e94d79f4323508da35984ebb68f60f4d0
Vern Anderson <keencommander@hotmail.com>
2024-10-22 15:28:30 -0500

    Add files via upload
    Got board so I made a little ASCII man walking

---
+++ Animate-LittleGreenMan.ps1
@@ -0,0 +1,24 @@
+﻿$Steps = 0..101
+foreach ($Step in $Steps)
+    {
+    Clear-Host
+    $Head = 'O'
+    if ($Step % 2 -eq 0)
+        {
+        $Arms = '↙|↘'
+        $Legs = '/\'
+        Write-Host -Object $Head.PadLeft($Step) -ForegroundColor Green
+        Write-Host -Object $Arms.PadLeft($Step) -ForegroundColor Green
+        Write-Host -Object $Legs.PadLeft($Step) -ForegroundColor Green
+        }
+    else
+        {
+        $Arms = '|'
+        $Legs = '‖'
+        Write-Host -Object $Head.PadLeft($Step) -ForegroundColor Green
+        Write-Host -Object $Arms.PadLeft($Step) -ForegroundColor Green
+        Write-Host -Object $Legs.PadLeft($Step) -ForegroundColor Green
+        }
+    Start-Sleep -Milliseconds 125
+    }
+$Step = 0
