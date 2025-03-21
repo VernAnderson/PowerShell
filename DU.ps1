39d44afd57627a1b0b52f591b2d178817da4d71d
Vern Anderson <keencommander@hotmail.com>
2025-03-21 09:36:13 -0500

    Update DU.ps1
    Making some grammatical changes, using full command name and not aliases and using the -Directory switch instead of the Where-object filtering to match only directories

--- DU.ps1
+++ DU.ps1
@@ -11,8 +11,8 @@
    I developed this script very early in my PowerShell learning and I still use it all the time. it is very useful especially when I have to tell people their hard drive is full. It helps them to see a summary oif what is using the space.
 #>
 $location = Get-Location
-Get-ChildItem $location | Where-Object { $_.PSIScontainer } |
-    Select @{Name="Size (MB)";Expression={(Get-ChildItem $_.Fullname -Recurse | 
+Get-ChildItem -Path $location -Directory |
+    Select-Object @{Name="Size (MB)";Expression={(Get-ChildItem $_.Fullname -Recurse | 
     Measure-Object -Property Length -Sum).Sum/1MB}},Fullname | Sort-Object "Size (MB)" -Descending | 
     Select-Object @{Name="Size (MB)";Expression={"{0:0,0.00}" -f ($_."Size (MB)")}},Fullname | 
     Format-Table -AutoSize
