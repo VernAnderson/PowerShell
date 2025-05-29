946972f7ca64d2c18838897b14192ca414bb5b91
Vern Anderson <keencommander@hotmail.com>
2025-05-29 13:10:47 -0500

    Update Get-LoggedOnUsers.ps1
    $Profile is a built in variable had to change that

--- Get-LoggedOnUsers.ps1
+++ Get-LoggedOnUsers.ps1
@@ -1,7 +1,7 @@
 $UserProfiles = Get-WmiObject -Query "SELECT * FROM Win32_UserProfile  WHERE Special != TRUE AND Loaded = TRUE"
-foreach ($Profile in $UserProfiles)
+foreach ($UserProfile in $UserProfiles)
     {
-    $UserID = $Profile.LocalPath.Split('\') | Select-Object -Last 1
+    $UserID = $UserProfile.LocalPath.Split('\') | Select-Object -Last 1
     $LoginTable = [ordered]@{'LoginID'=$UserID}
     $LoginID = New-Object -TypeName PSObject -Property $LoginTable
     Write-Output -InputObject $LoginID
